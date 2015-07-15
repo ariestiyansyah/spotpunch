40e0f159dfe6947d63126b5e8d2a56c8c0c1672d
Aji Achmad Mustofa <micro280991@gmail.com>
2015-07-15 21:46:25 +0700

    Add Base Spotpunch Administrator

--- app/controllers/administrators_controller.rb
+++ app/controllers/administrators_controller.rb
@@ -1,3 +1,32 @@
 class AdministratorsController < ApplicationController
   layout "administrator"
+
+  def index
+    
+  end
+
+  def users
+    
+  end
+
+  def malls
+    
+  end
+
+  def stores
+    
+  end
+
+  def products
+    
+  end
+
+  def promotions
+    
+  end
+
+  def setting
+    
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,55 +3,14 @@ Rails.application.routes.draw do
   root 'home#index'
 
   resources :administrators, only: [:index]  do
-
+    get 'users'                       => 'administrators#users',                on: :collection
+    get 'malls'                       => 'administrators#malls',                on: :collection
+    get 'stores'                      => 'administrators#stores',               on: :collection
+    get 'products'                    => 'administrators#products',             on: :collection
+    get 'promotions'                  => 'administrators#promotions',           on: :collection
+    get 'setting'                     => 'administrators#setting',              on: :collection
+    get 'import'                      => 'administrators#import',               on: :collection
+    collection { post :import }
   end
 
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
-
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
-
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
 end
