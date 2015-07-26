41e54e205ac4512bd642b595af7f8cbae098b1fa
Aji Achmad Mustofa <micro280991@gmail.com>
2015-07-26 17:36:37 +0700

    Registration System Version 1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,11 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def authenticate_user!
+    @user = User.new
+    unless user_signed_in?
+      render "users/sessions/new"
+    end
+  end
 end
