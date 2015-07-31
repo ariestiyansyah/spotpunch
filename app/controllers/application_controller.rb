8bb5c5bd8bdf630cf8eb4e43dcccd574d1dddee5
Aji Achmad Mustofa <micro280991@gmail.com>
2015-08-01 04:43:46 +0700

    Fix Merge Timeline

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  def authenticate_user!
+  def authenticate_user! opts={}
     @user = User.new
     unless user_signed_in?
       render "users/sessions/new"
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def display_name user
+    if user.firstname.blank? && user.lastname.blank?
+      user.email
+    else
+      "#{user.firstname} #{user.lastname}"
+    end
+  end
 end
